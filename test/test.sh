ec41e8498c358948c32aa078b138b32b2d83b465
8Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-05-27 13:49:39 +0700

    Add files via upload

---
+++ test/test.sh
@@ -0,0 +1,32 @@
+#lsblk
+lsblk
+#iwctl
+iwctl
+#конект к интернету
+station wlan0 connect vivo_V25e
+#форматирую диск
+mkfs.btrfs -f /dev/sdc1
+#mount mnt
+mount /dev/sdc1 /mnt
+#cd /mnt
+cd /mnt
+#su cr
+btrfs su cr @
+#su cr
+btrfs su cr @home
+#su cr
+btrfs su cr @var
+#cd
+cd
+#umount
+umount /mnt
+#mount disk
+mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@ /dev/sdc1 /mnt
+#mkdir folders
+mkdir /mnt/{var,home,boot}
+#mkdir /boot/efi
+mkdir /mnt/boot/efi
+#mount disk
+mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@home /dev/sdc1 /mnt/home
+#mount disk
+mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@var /dev/sdc1 /mnt/var
