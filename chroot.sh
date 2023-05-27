40a8dd5b02b4ea93d7a3c75278101c4e511806a6
8Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-05-27 22:04:35 +0700

    install3.sh

--- chroot.sh
+++ chroot.sh
@@ -3,19 +3,15 @@
 username=neo
 pass=1811
 
-#services
-systemctl enable iwd.service
-#services
-systemctl enable NetworkManager.service
-#install xorg
+#xorg
 sudo pacman –S xorg-server
 #timezone
 sudo timedatectl set-timezone Asia/Krasnoyarsk
 #nvidia
 sudo pacman -S nvidia-dkms nvidia-utils opencl-nvidia nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils
-#install kde and drivers
+#kde and drivers
 sudo pacman –S dolphin dolphin-plugins konsole plasma plasma-desktop kdenlive gwenview elisa mpv kcalc kcalendarcore kdeconnect plasma-wayland-session kwalletmanager sddm bluez-utils  libva-vdpau-driver libva-utils vdpauinfo vulkan-icd-loader lib32-vulkan-icd-loader vulkan-headers vulkan-validation-layers vulkan-tools pipewire lib32-pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack qt-gstreamer kdenetwork-filesharing spectacle
-#install apps
+#apps
 sudo pacman -S firefox gparted engrampa caja p7zip unace brotli rpm-tools cpio latte-dock steam steam-native-runtime obs-studio cuda blender krita qbittorrent telegram-desktop gnome-disk-utility cpupower bash icoutils wget bubblewrap zstd cabextract bc tar openssl gamemode desktop-file-utils curl dbus freetype2 gdk-pixbuf2 ttf-font zenity lsb-release nss xorg-xrandr vulkan-driver lsof lib32-freetype2 lib32-libgl lib32-gcc-libs lib32-libx11 lib32-libxss lib32-alsa-plugins lib32-libgpg-error lib32-nss lib32-vulkan-driver lib32-gamemode lib32-openssl meson gst-libav gst-plugins-bad gst-plugins-base gst-plugins-good gst-plugins-ugly ffnvcodec-headers samba cifs-utils extra-cmake-modules plasma-framework gst-libav base-devel mpv python-websockets qt5-declarative qt5-websockets qt5-webchannel cmake
 #setting cpupower
 sudo cpupower frequency-set -g performance
@@ -33,9 +29,9 @@ cd pikaur
 makepkg -fsri
 #cd
 cd
-#install apps
+#aur apps
 pikaur -S libva-nvidia-driver-git
-#install apps
+#aur apps
 pikaur -S pamac-aur stacer-bin fastfetch timeshift timeshifs-autosnap protonup-qt-bin google-chrome yandex-browser speech-dispatcher ttf-font gstreamer-meta ventoy-bin onlyoffice-bin xow-git --noconfirm
 #services
 sudo systemctl enable xow.service
@@ -56,4 +52,4 @@ echo 'GST_VAAPI_ALL_DRIVERS=1' >> /etc/enviroment
 #mkinitcpio
 sudo mkinitcpio -P
 #exit
-reboot
+exit
