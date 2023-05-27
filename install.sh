8cc96592f0789b12228722b90d06d14efaf26d6d
8Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-05-27 21:53:53 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,19 @@ echo '--------------------------------------------------------------------------
 username=neo
 hostname=matrix
 pass=1811
+disk_root=/dev/sdc3
+disk_boot=/dev/sdc2
 
 echo '--------------------------------------------------------------------------------'
 echo ' Install Arch Linux '$username'@'$hostname
 echo '--------------------------------------------------------------------------------'
 
 #formating disk
-mkfs.btrfs -f /dev/sdc3
+mkfs.btrfs -f $disk_root
 #formating disk
-mkfs.vfat /dev/sdc2
+mkfs.vfat $disk_boot
 #mount mnt
-mount /dev/sdc3 /mnt
+mount $disk_root /mnt
 #cd /mnt
 cd /mnt
 #su cr
@@ -30,15 +32,15 @@ cd
 #umount
 umount /mnt
 #mount disk
-mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@ /dev/sdc3 /mnt
+mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@ $disk_root /mnt
 #mkdir folders
 mkdir /mnt/{var,home,boot}
 #mkdir /boot/efi
 mkdir /mnt/boot/efi
 #mount disk
-mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@home /dev/sdc3 /mnt/home
+mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@home $disk_root /mnt/home
 #mount disk
-mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@var /dev/sdc3 /mnt/var
+mount -o noatime,compress=zstd:3,space_cache=v2,discard=async,subvol=@var $disk_root /mnt/var
 #install arch
 pacstrap -K /mnt base base-devel linux-firmware refind efibootmgr iwd networkmanager micro htop btrfs-progs git intel-ucode zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions grml-zsh-config iucode-tool --noconfirm
 #fstab generation
