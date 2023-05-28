2eeee543be2ab4284b73a755bf56502eb27aa09d
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-05-28 09:59:11 +0700

    install3.sh

--- install3.sh
+++ install3.sh
@@ -75,6 +75,8 @@ sudo gpasswd users -a $username
 sudo systemctl enable smb.service
 #samba
 sudo systemctl enable nmb.service
+#portproton
+wget -c "https://github.com/Castro-Fidel/PortWINE/raw/master/portwine_install_script/PortProton_1.0" && sh PortProton_1.0 -rus
 #mkinitcpio
 sudo mkinitcpio -P
 #exit
