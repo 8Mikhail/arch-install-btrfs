f137d167a579f5d828335b05114eeb673ea95681
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-05-31 00:55:06 +0700

    install3.sh

--- install3.sh
+++ install3.sh
@@ -22,7 +22,7 @@ sudo pacman -S mesa --noconfirm
 #pipewire
 sudo pacman -S pipewire lib32-pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack --noconfirm
 #KDE and apps
-sudo pacman -S plasma plasma-desktop --noconfirm
+sudo pacman -S plasma plasma-desktop packagekit-qt5 --noconfirm
 #KDE drivers and apps
 sudo pacman -S dolphin dolphin-plugins konsole kdenlive gwenview elisa mpv kcalc kcalendarcore kdeconnect plasma-wayland-session kwalletmanager libva-utils vdpauinfo vulkan-icd-loader lib32-vulkan-icd-loader vulkan-headers vulkan-validation-layers vulkan-tools qt-gstreamer kdenetwork-filesharing spectacle --noconfirm
 #apps
