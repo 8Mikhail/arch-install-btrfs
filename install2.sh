728c5a2b810cf8340a2e28747d1052215a2d5cca
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-05-31 00:31:44 +0700

    install2.sh

--- install2.sh
+++ install2.sh
@@ -46,6 +46,8 @@ useradd -m -g users -G wheel,video -s /usr/bin/zsh $username
 ) | passwd $username
 #sudoers
 sed 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' -i /etc/sudoers
+#chown
+chown -R $username:users /home/$username/
 #install core lqx
 pacman-key --keyserver hkps://keyserver.ubuntu.com --recv-keys 9AE4078033F8024D
 #install core lqx
