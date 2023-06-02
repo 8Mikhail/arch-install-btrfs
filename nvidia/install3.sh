ea29dc43e72fa0e34ab5b17913077ea09eae7c53
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-06-02 17:56:00 +0700

    install3.sh

--- nvidia/install3.sh
+++ nvidia/install3.sh
@@ -45,8 +45,6 @@ function aur {
 aur libva-nvidia-driver-git pamac-aur stacer-bin fastfetch timeshift timeshift-autosnap protonup-qt-bin google-chrome yandex-browser speech-dispatcher ttf-font gstreamer-meta ventoy-bin onlyoffice-bin xow-git --noconfirm
 #aur apps and opencl-amd:
 #aur opencl-amd pamac-aur stacer-bin fastfetch timeshift timeshift-autosnap protonup-qt-bin google-chrome yandex-browser speech-dispatcher ttf-font gstreamer-meta ventoy-bin onlyoffice-bin xow-git --noconfirm
-#services:
-sudo systemctl enable xow
 #portproton:
 #cd /home/$username/:
 cd /home/$username/
