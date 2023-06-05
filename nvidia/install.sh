b3426982884689cdd806a53066ff39fe5c077e67
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-06-05 18:08:38 +0700

    install.sh

--- nvidia/install.sh
+++ nvidia/install.sh
@@ -91,7 +91,7 @@ cp -f arch-install-btrfs/nvidia/install2.sh /mnt/home/$username/
 #sleep:
 sleep $sleep
 #chroot /mnt:
-arch-chroot /mnt sh -c "$(cat /mnt/home/$username/install2.sh)" $username $hostname $pass
+arch-chroot /mnt sh -c "$(cat /home/$username/install2.sh)" $username $hostname $pass
 #sleep:
 sleep $sleep
 echo '--------------------------------------------------'
