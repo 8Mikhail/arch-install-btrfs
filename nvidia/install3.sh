21535d8e5cf5e1b0a80064d2dbcdb45845cdb044
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-08-07 22:54:56 +0700

    install3.sh

--- nvidia/install3.sh
+++ nvidia/install3.sh
@@ -30,7 +30,7 @@ echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 #
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 #
-sudo sed 's|#export PATH=$HOME/bin:/usr/local/bin:$PATH|export PATH=$HOME/bin:/usr/local/bin:$PATH|g' -i ~/.zshrc
+sudo sed 's|# export PATH=$HOME/bin:/usr/local/bin:$PATH|export PATH=$HOME/bin:/usr/local/bin:$PATH|g' -i ~/.zshrc
 #
 sudo sed 's|plugins=(git)|plugins=(git zsh-autosuggestions sudo)|g' -i .zshrc
 #
