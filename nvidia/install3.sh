6cb042f7c56582834257b1fca9da75714850ea67
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-08-07 22:35:55 +0700

    install3.sh

--- nvidia/install3.sh
+++ nvidia/install3.sh
@@ -11,22 +11,30 @@ nvidia_no_vdpau_nvidia=nvidia-dkms nvidia-utils opencl-nvidia nvidia-settings li
 echo '--------------------------------------------------'
 echo '|            Установка оболочки zsh              |'
 echo '--------------------------------------------------'
-#zsh:
-(
-    echo $pass
-) | sudo pacman -S zsh zsh-completions zsh-syntax-highlighting zsh-autosuggestions grml-zsh-config --noconfirm
-#zsh chsh user:
-chsh -s /usr/bin/zsh
-#zsh chsh root:
-(
-    echo $pass
-) | sudo chsh -s /usr/bin/zsh
-#zsh:
-wget 'https://raw.githubusercontent.com/like913/arch-install/master/config/.zshrc'
-#chown:
-(
-    echo $pass
-) | sudo chown -R $username:users .zshrc
+#zsh
+sudo pacman -S zsh --noconfirm
+#oh-my-zsh
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+#chsh
+(  echo $pass 
+   echo $pass ) | chsh -s /usr/bin/zsh
+#chsh
+(  echo $pass 
+   echo $pass ) | sudo chsh -s /usr/bin/zsh
+#
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+#
+mv zsh-syntax-highlighting .zsh-syntax-highlighting
+#
+echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+#
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#
+sudo sed 's|#export PATH=$HOME/bin:/usr/local/bin:$PATH|export PATH=$HOME/bin:/usr/local/bin:$PATH|g' -i ~/.zshrc
+#
+sudo sed 's|plugins=(git)|plugins=(git zsh-autosuggestions sudo)|g' -i .zshrc
+#
+source .zshrc
 echo '--------------------------------------------------'
 echo '|              Установка драйверов               |'
 echo '--------------------------------------------------'
