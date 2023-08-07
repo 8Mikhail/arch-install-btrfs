d1769c11614521c79b782bd8a5295d19b2c6d16c
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-08-07 22:37:18 +0700

    install3.sh

--- amd/install3.sh
+++ amd/install3.sh
@@ -7,6 +7,34 @@ amd=mesa lib32-mesa libva-mesa-driver lib32-libva-mesa-driver
 nvidia=nvidia-dkms nvidia-utils opencl-nvidia nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils cuda libva-vdpau-driver lib32-libva-vdpau-driver
 nvidia_no_vdpau_nvidia=nvidia-dkms nvidia-utils opencl-nvidia nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils cuda
 
+#install zsh:
+echo '--------------------------------------------------'
+echo '|            Установка оболочки zsh              |'
+echo '--------------------------------------------------'
+#zsh
+sudo pacman -S zsh --noconfirm
+#oh-my-zsh
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+#chsh
+(  echo $pass 
+   echo $pass ) | chsh -s /usr/bin/zsh
+#chsh
+(  echo $pass 
+   echo $pass ) | sudo chsh -s /usr/bin/zsh
+#
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+#
+mv zsh-syntax-highlighting .zsh-syntax-highlighting
+#
+echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+#
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+#
+sudo sed 's|#export PATH=$HOME/bin:/usr/local/bin:$PATH|export PATH=$HOME/bin:/usr/local/bin:$PATH|g' -i ~/.zshrc
+#
+sudo sed 's|plugins=(git)|plugins=(git zsh-autosuggestions sudo)|g' -i .zshrc
+#
+source .zshrc
 echo '--------------------------------------------------'
 echo '|              Установка драйверов               |'
 echo '--------------------------------------------------'
@@ -57,30 +85,6 @@ cd /home/$username/
 wget -c "https://github.com/Castro-Fidel/PortWINE/raw/master/portwine_install_script/PortProton_1.0" && sh PortProton_1.0 rus
 #cd:
 cd
-#zsh
-sudo pacman -S zsh --noconfirm
-#oh-my-zsh
-curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-#chsh
-(  echo $pass 
-   echo $pass ) | chsh -s /usr/bin/zsh
-#chsh
-(  echo $pass 
-   echo $pass ) | sudo chsh -s /usr/bin/zsh
-#
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-#
-mv zsh-syntax-highlighting .zsh-syntax-highlighting
-#
-echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-#
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#
-sudo sed 's|#export PATH=$HOME/bin:/usr/local/bin:$PATH|export PATH=$HOME/bin:/usr/local/bin:$PATH|g' -i ~/.zshrc
-#
-sudo sed 's|plugins=(git)|plugins=(git zsh-autosuggestions sudo)|g' -i .zshrc
-#
-source .zshrc
 #mkinitcpio.conf:
 #Раскомментируйте на необходимое:
 #nvidia modules:
