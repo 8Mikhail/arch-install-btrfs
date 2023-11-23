d20382f5cc8702363a3814a80c9396301bada0e1
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-11-23 17:41:55 +0700

    flash_install3.sh

--- flash_install3.sh
+++ flash_install3.sh
@@ -7,34 +7,7 @@ amd=mesa lib32-mesa libva-mesa-driver lib32-libva-mesa-driver
 nvidia=nvidia-dkms nvidia-utils opencl-nvidia nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils cuda libva-vdpau-driver lib32-libva-vdpau-driver
 nvidia_no_vdpau_nvidia=nvidia-dkms nvidia-utils opencl-nvidia nvidia-settings lib32-opencl-nvidia lib32-nvidia-utils cuda
 
-#install zsh:
-echo '--------------------------------------------------'
-echo '|            Установка оболочки zsh              |'
-echo '--------------------------------------------------'
-#zsh
-sudo pacman -S zsh --noconfirm
-#oh-my-zsh
-curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-#chsh
-(  echo $pass 
-   echo $pass ) | chsh -s /usr/bin/zsh
-#chsh
-(  echo $pass 
-   echo $pass ) | sudo chsh -s /usr/bin/zsh
-#
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-#
-mv zsh-syntax-highlighting .zsh-syntax-highlighting
-#
-echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-#
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#
-sudo sed 's|# export PATH=$HOME/bin:/usr/local/bin:$PATH|export PATH=$HOME/bin:/usr/local/bin:$PATH|g' -i ~/.zshrc
-#
-sudo sed 's|plugins=(git)|plugins=(git zsh-autosuggestions sudo)|g' -i .zshrc
-#
-source .zshrc
+
 echo '--------------------------------------------------'
 echo '|              Установка драйверов               |'
 echo '--------------------------------------------------'
