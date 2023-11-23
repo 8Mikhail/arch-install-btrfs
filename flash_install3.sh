340babdb80cfb715860a5d63d775ad368e368af1
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-11-23 17:46:43 +0700

    flash_install3.sh

--- flash_install3.sh
+++ flash_install3.sh
@@ -36,8 +36,6 @@ sudo pacman -S bluez bluez-utils --noconfirm
 sudo pacman -S cpupower --noconfirm
 #setting cpupower:
 sudo cpupower frequency-set -g performance
-#install portproton:
-wget -c "https://github.com/Castro-Fidel/PortWINE/raw/master/portwine_install_script/PortProton_1.0" && sh PortProton_1.0 rus
 #mkinitcpio.conf:
 #Раскомментируйте на необходимое:
 #nvidia modules:
