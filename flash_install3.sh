b770f68618c296a4af9fa39b22214992583c10db
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-11-24 16:48:11 +0700

    flash_install3.sh

--- flash_install3.sh
+++ flash_install3.sh
@@ -44,6 +44,8 @@ sudo cpupower frequency-set -g performance
 #Раскомментируйте на необходимое:
 #nvidia modules:
 sudo sed -i 's|MODULES=()|MODULES="nvidia nvidia_modeset nvidia_uvm nvidia_drm btrfs"|g' /etc/mkinitcpio.conf
+#grub:
+see -i 's|GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"|GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia-drm.modeset=1"|g' /etc/default/grub
 #enviroment:
 sudo echo 'NVD_BACKEND=direct' >> /etc/enviroment
 #enviroment:
