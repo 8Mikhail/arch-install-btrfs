44e2e78af3d8a619462c598f601b49211929098e
Mikhail <134766340+8Mikhail@users.noreply.github.com>
2023-11-25 22:20:59 +0700

    install wallpaper engine.sh

---
+++ install wallpaper engine.sh
@@ -0,0 +1,19 @@
+# Download source
+git clone https://github.com/catsout/wallpaper-engine-kde-plugin.git
+cd wallpaper-engine-kde-plugin
+
+# Download submodule (glslang)
+git submodule update --init
+
+# Configure
+# 'USE_PLASMAPKG=ON': using plasmapkg2 tool to install plugin
+mkdir build && cd build
+cmake .. -DUSE_PLASMAPKG=ON
+
+# Build
+make -j$nproc
+
+# Install package (ignore if USE_PLASMAPKG=OFF for system-wide installation)
+make install_pkg
+# install lib
+sudo make install
